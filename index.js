e7e07f1fcad472876464574421481497c5a36433
Henrique Paulo Bonfim <40275173+hpbonfim@users.noreply.github.com>
2023-04-21 22:47:15 +0000

    adicionando index.html com hello world

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 
+// Configurações do Express para servir arquivos estáticos
+app.use(express.static('public'));
+
 // Rota inicial
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/', (request, response) => {
+  response.sendFile(__dirname + '/public/index.html');
 });
 
 // Inicia o servidor na porta 3000
